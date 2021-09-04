352014e54514c159ee73de9ca3eb53ecd520eddc
James <jamesschmidt124@gmail.com>
2021-09-03 20:23:23 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names) {
+  const cards = [];
+        for (let i = 0; i < names.length; i++) {
+             cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        }
+        return cards;
+         }
+function countDown () {
+    let countDown = 10;
+    while (countDown > -1) {
+      console.log(countDown--);
+    } 
+}
